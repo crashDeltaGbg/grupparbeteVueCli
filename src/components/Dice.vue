cf15bf9fd7e5da746837279b7fcbfd13b5c1ad19
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-06 00:40:48 +0100

    Add component 'Dice'

--- src/components/Dice.vue
+++ src/components/Dice.vue
@@ -5,27 +5,31 @@
 <script>
 
 export default {
+  created() {
+    console.log(this.die)
+  },
   data() {
     return {
-      face: null,
+      face: 'roll',
       dice: {
         D4: {min: 1, max: 4, multiplier: 1},
         D6: {min: 1, max: 6, multiplier: 1},
         D8: {min: 1, max: 8, multiplier: 1},
-        D10: {min: 0, max: 9, multiplier: 1},
+        D10: {min: 0, max: 10, multiplier: 1},
         D12: {min: 1, max: 12, multiplier: 1},
         D20: {min: 1, max: 20, multiplier: 1},
-        D100: {min: 0, max: 9, multiplier: 10}
+        D100: {min: 0, max: 10, multiplier: 10}
       }
     }
   },
   methods: {
     roll() {
-      this.face = (Math.floor(Math.random() * Number(this.dice.die.max)) + Number(this.dice.die.min)) * Number(this.dice.die.multiplier)
+      this.face = (Math.floor(Math.random() * Number(this.dice[this.type].max)) + Number(this.dice[this.type].min)) * Number(this.dice[this.type].multiplier)
+      console.log(this.face)
     }
   },
   name: 'dice',
-  props: ['die']
+  props: ['type']
 }
 
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
+    <Dice type="D4"/> D4<br/>
+    <Dice type="D6"/> D6<br/>
+    <Dice type="D8"/> D8<br/>
+    <Dice type="D10"/> D10<br/>
+    <Dice type="D12"/> D12<br/>
+    <Dice type="D20"/> D20<br/>
+    <Dice type="D100"/> D10-00-90
     <!-- <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
@@ -7,7 +14,7 @@
 
 <script>
 
-import Dice from './Dice.vue'
+import Dice from '@/components/Dice.vue'
 
 export default {
   name: 'Home',
