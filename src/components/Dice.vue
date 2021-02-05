62a3cad86913c3b35db54588e29d0018787bbb5e
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-05 19:53:43 +0100

    First commit

---
+++ src/components/Dice.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="button" @click="roll()" :value="face"/>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      face: null,
+      dice: {
+        D4: {min: 1, max: 4, multiplier: 1},
+        D6: {min: 1, max: 6, multiplier: 1},
+        D8: {min: 1, max: 8, multiplier: 1},
+        D10: {min: 0, max: 9, multiplier: 1},
+        D12: {min: 1, max: 12, multiplier: 1},
+        D20: {min: 1, max: 20, multiplier: 1},
+        D100: {min: 0, max: 9, multiplier: 10}
+      }
+    }
+  },
+  methods: {
+    roll() {
+      this.face = (Math.floor(Math.random() * Number(this.dice.die.max)) + Number(this.dice.die.min)) * Number(this.dice.die.multiplier)
+    }
+  },
+  name: 'dice',
+  props: ['die']
+}
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+
+import Dice from './Dice.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Dice
+  }
+}
+</script>
