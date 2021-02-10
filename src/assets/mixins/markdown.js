92065c7845d133785db0c865871cf149316d9899
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-10 20:43:26 +0100

    Rename folder scripts to mixins

---
+++ src/assets/mixins/markdown.js
@@ -0,0 +1,19 @@
+export const markdown = {
+  created() {
+    // selectFile(test)
+    // console.log(this.fileName)
+    // this.markdown = require(`@/assets/story/${this.fileName}.md`)
+  },
+  data() {
+    return {
+      markdown
+    }
+  },
+  methods: {
+    selectFile(fileName) {
+      this.markdown = require(`@/assets/story/${fileName}.md`)
+    }
+  },
+  name: 'markdown',
+  // props: ['fileName']
+  }
