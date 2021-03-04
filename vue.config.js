5352aadc4edd1a45a55db56a388bc7b428c0d7d4
Hankro93 <hanna.kron@lokron.com>
2021-03-04 15:59:29 +0100

    added kanye west api to vueconfig file

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,17 @@ module.exports = {
       background_color: 'darkgrey'
     },
     name: 'Adventure Hunter',
-    themeColor: 'darkgreen'
-    /* workboxOptions: {
-      manifestTransforms: [
-        manifest => ({
-          manifest: manifest.concat([
-            { revision: 0, url: '/story/json/story.json' }
-          ])
-        })
+    themeColor: 'darkgreen',
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          handler: 'NetworkFirst',
+          options: {
+            networkTimeoutSeconds: 5
+          },
+          urlPattern: 'https://api.kanye.rest'
+        }
       ]
-    } */
+    }
   }
 }
