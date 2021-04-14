96551a3f0fd9381141293980ad787547a1efe476
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-04-14 15:37:40 +0200

    set publicPath in vue.config.js

--- src/assets/mixins/measure.js
+++ src/assets/mixins/measure.js
@@ -1,15 +1,7 @@
 export const measure = {
-  /*computed: {
-    character() {
-      return this.$store.state.character
-    }
-  },*/
   methods: {
     measure(stat) {
       let score = this.roll(this.die) + this.effectiveStats()[stat]
-      /*if (this.character.equipment) {
-        score += this.character.equipment.stats[stat]
-      }*/
       let target = this.roll(this.die) + Number(this.success)
       if (score >= target) {
         this.getStory(this.options[0].proceed)
